2cb66d46e6002c279bcf342b998080427405c911
Bokle Zhu <Bokle@foxmail.com>
2021-07-12 21:43:13 +0800

    0.2

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ var config = {
     }
 }
 var game = new Phaser.Game(config);
-var people0,people1,people2,people3;
+var people0,people1,people2,people3,car1,car2,car3,car4;
 
 function preload() {
 
@@ -45,7 +45,8 @@ function preload() {
     
 }
 
-var posx0, posy0, posx1, posy1, posx2, posy2, posx3, posy3;
+var posx0, posy0, posx1, posy1, posx2, posy2, posx3, posy3,
+    carx1,cary1,carx2,cary2,carx3,cary3,carx4,cary4;
 function create() {
     this.add.image(480, 320, 'bg');//x:155-805   y:150-650
     //用random函数生成随机坐标，用于放置单位
@@ -88,8 +89,56 @@ function create() {
     people3=this.add.sprite(posx3, posy3, 'p3');
  
     //var pos0 = { x: posx0, y: posy0 }, pos1 = { x: posx1, y: posy1 }, pos2 = { x: posx2, y: posy2 }, pos3 = { x: posx3, y: posy3 };
-    
+    carx1 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
+    cary1 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
+    while (abs(carx1 - posx0) <= 75 && abs(cary1 - posy0) <= 75
+        || abs(carx1 - posx1) <= 75 && abs(cary1 - posy1) <= 75
+        || abs(carx1 - posx2) <= 75 && abs(cary1 - posy2) <= 75
+        || abs(carx1 - posx3) <= 75 && abs(cary1 - posy3) <= 75    ) {
+        carx1 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
+        cary1 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
+    }
+    car1 = this.add.sprite(carx1, cary1, 'c1');
+
+    carx2 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
+    cary2 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
+    while (abs(carx2 - posx0) <= 75 && abs(cary2 - posy0) <= 75
+        || abs(carx2 - posx1) <= 75 && abs(cary2 - posy1) <= 75
+        || abs(carx2 - posx2) <= 75 && abs(cary2 - posy2) <= 75
+        || abs(carx2 - posx3) <= 75 && abs(cary2 - posy3) <= 75
+        || abs(carx2 - carx1) <= 75 && abs(cary2 - cary1) <= 75    ) {
+        carx2 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
+        cary2 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
+    }
+    car2 = this.add.sprite(carx2, cary2, 'c2');
+
+    carx3 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
+    cary3 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
+    while (abs(carx3 - posx0) <= 75 && abs(cary3 - posy0) <= 75
+        || abs(carx3 - posx1) <= 75 && abs(cary3 - posy1) <= 75
+        || abs(carx3 - posx2) <= 75 && abs(cary3 - posy2) <= 75
+        || abs(carx3 - posx3) <= 75 && abs(cary3 - posy3) <= 75
+        || abs(carx3 - carx1) <= 75 && abs(cary3 - cary1) <= 75
+        || abs(carx3 - carx2) <= 75 && abs(cary3 - cary2) <= 75    ) {
+        carx3 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
+        cary3 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
+    }
+    car3 = this.add.sprite(carx3, cary3, 'c3');
+
+    carx4 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
+    cary4 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
+    while (abs(carx4 - posx0) <= 75 && abs(cary4 - posy0) <= 75
+        || abs(carx4 - posx1) <= 75 && abs(cary4 - posy1) <= 75
+        || abs(carx4 - posx2) <= 75 && abs(cary4 - posy2) <= 75
+        || abs(carx4 - posx3) <= 75 && abs(cary4 - posy3) <= 75
+        || abs(carx4 - carx1) <= 75 && abs(cary4 - cary1) <= 75
+        || abs(carx4 - carx2) <= 75 && abs(cary4 - cary2) <= 75
+        || abs(carx4 - carx3) <= 75 && abs(cary4 - cary3) <= 75    ) {
+        carx4 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
+        cary4 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
+    }
+    car4 = this.add.sprite(carx4, cary4, 'c4');
 }
 function update(time,delta) {
-    people0.x += 1;//这里写每个精灵对象的位置操作 在很小的时间间隔内执行一次这个函数
+    people1.x += 1;//这里写每个精灵对象的位置操作 在很小的时间间隔内执行一次这个函数
 }
