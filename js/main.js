50f8b708d5ec0bc4681f49a2617324afbe71c013
Bokle Zhu <Bokle@foxmail.com>
2021-07-13 22:21:43 +0800

    0.4

--- js/main.js
+++ js/main.js
@@ -14,11 +14,29 @@ function abs(j)
 //引入Math对象使用Math.abs()能达到一样的效果
 
 
-function update_direction(iter)//更新第iter个元素的方向  仅能更改为上下左右
+function reset_direction(iter)//更新第iter个元素的方向  仅能更改为上下左右
 {
     direction[iter] = Math.floor(Math.random() * 4) * 90;
 }
 
+function update_direction(iter) {
+    if(iter==0)
+        people0.angle = direction[0];
+    if (iter == 1)
+        people1.angle = direction[1];
+    if (iter == 2)
+        people2.angle = direction[2];
+    if (iter == 3)
+        people3.angle = direction[3];
+    if (iter == 4)
+        car1.angle = direction[4];
+    if (iter == 5)
+        car2.angle = direction[5];
+    if (iter == 6)
+        car3.angle = direction[6];
+    if (iter == 7)
+        car4.angle = direction[7];
+}
 
 var config = {
     width: 960,
@@ -60,20 +78,34 @@ var posx0, posy0, posx1, posy1, posx2, posy2, posx3, posy3,
 
 let direction = [ 0, 0, 0, 0, 0, 0, 0, 0];
 //分别为p0,p1,p2,p3,c1,c2,c3,c4的方向 默认为0 朝上
+
+//let buttons = new Array(8);
+//使用预定义的Array对象中的构造函数创建名为buttons的长度为8的数组用于存放
+
+let movable = [false, false, false, false, false, false, false, false];
+
 function create() {
     this.add.image(480, 320, 'bg');//x:155-805   y:150-650
     //用random函数生成随机坐标，用于放置单位
     //用random函数生成四个随机方向，用于确定精灵对象的方向
     for (let i = 0; i < 8; i++) {
-        update_direction(i);
+        reset_direction(i);
         //console.log(direction[i]);
     }
+    /*
+    this.input.onDown.add(function (pointer) {
+       
+    });
+    */
+
+
     posx0 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
     posy0 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
     people0=this.add.sprite(posx0, posy0, 'p0'); 
-    people0.angle = direction[0];
+    update_direction(0);
+    //buttons[0] = this.add.button(posx0, posy0, 'butp', function () { movable[0] = true; }, this, 0, 0, 0, 0);
     //设置透明按钮和人重合
-
+    //车同理
     posx1 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
     posy1 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
     while (abs(posx1 - posx0) <= 75 && abs(posy1 - posy0) <= 75)
@@ -82,8 +114,8 @@ function create() {
         posy1 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
     }
     people1 = this.add.sprite(posx1, posy1, 'p1');
-    people1.angle = direction[1];
-
+    update_direction(1);
+    //buttons[1] = this.add.button(posx1, posy1, 'butp', function () { movable[1] = true; }, this, 0, 0, 0, 0);
 
 
     posx2 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
@@ -96,8 +128,8 @@ function create() {
     }
 
     people2 = this.add.sprite(posx2, posy2, 'p2');
-    people2.angle = direction[2];
-
+    update_direction(2);
+    //buttons[2] = this.add.button(posx2, posy2, 'butp', function () { movable[2] = true; }, this, 0, 0, 0, 0);
 
 
     posx3 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
@@ -110,11 +142,11 @@ function create() {
         posy3 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
     }
     people3=this.add.sprite(posx3, posy3, 'p3');
-    people3.angle = direction[3];
+    update_direction(3);
+    //buttons[3] = this.add.button(posx3, posy3, 'butp', function () { movable[3] = true; }, this, 0, 0, 0, 0);
 
 
-
-    //var pos0 = { x: posx0, y: posy0 }, pos1 = { x: posx1, y: posy1 }, pos2 = { x: posx2, y: posy2 }, pos3 = { x: posx3, y: posy3 };
+    
     carx1 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
     cary1 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
     while (abs(carx1 - posx0) <= 100 && abs(cary1 - posy0) <= 100
@@ -125,9 +157,9 @@ function create() {
         cary1 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
     }
     car1 = this.add.sprite(carx1, cary1, 'c1');
-    car1.angle = direction[4];
-
-
+    update_direction(4);
+    //buttons[4] = this.add.button(carx1, cary1, 'butc', function () { movable[4] = true; }, this, 0, 0, 0, 0);
+    //buttons[4].angle = direction[4];
 
     carx2 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
     cary2 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
@@ -140,8 +172,9 @@ function create() {
         cary2 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
     }
     car2 = this.add.sprite(carx2, cary2, 'c2');
-    car2.angle = direction[5];
-
+    update_direction(5);
+    //buttons[5] = this.add.button(carx2, cary2, 'butc', function () { movable[5] = true; }, this, 0, 0, 0, 0);
+    //buttons[5].angle = direction[5];
 
     carx3 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
     cary3 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
@@ -155,8 +188,9 @@ function create() {
         cary3 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
     }
     car3 = this.add.sprite(carx3, cary3, 'c3');
-    car3.angle = direction[6];
-
+    update_direction(6);
+    //buttons[6] = this.add.button(carx3, cary3, 'butc', function () { movable[6] = true; }, this, 0, 0, 0, 0);
+    //buttons[6].angle = direction[6];
 
 
     carx4 = 480 + 320 * (Math.round(Math.random() * 1000) / 500 - 1);
@@ -172,17 +206,53 @@ function create() {
         cary4 = 320 + 170 * (Math.round(Math.random() * 1000) / 500 - 1);
     }
     car4 = this.add.sprite(carx4, cary4, 'c4');
-    car4.angle = direction[7];
-
+    update_direction(7);
+    //buttons[7] = this.add.button(carx4, cary4, 'butc', function () { movable[7] = true; }, this, 0, 0, 0, 0);
+    //buttons[7].angle = direction[7];
 
 }
 
  
 
-function update(time,delta) {
-    people1.x += 0;//这里写每个精灵对象的位置操作 在很小的时间间隔内执行一次这个函数
+function update(time, delta) {
+    var pointer = this.input.activePointer;
+    var x = pointer.worldX,y = pointer.worldY;
+    if (pointer.isDown) {//监听鼠标的点击，同时判断出鼠标点击的对象
+        if (abs(x - people0.x) < people0.width / 2 && abs(y - people0.y) < people0.height / 2)
+            movable[0] = true;
+        if (abs(x - people1.x) < people1.width / 2 && abs(y - people1.y) < people1.height / 2)
+            movable[1] = true;
+        if (abs(x - people2.x) < people2.width / 2 && abs(y - people2.y) < people2.height / 2)
+            movable[2] = true;
+        if (abs(x - people3.x) < people3.width / 2 && abs(y - people3.y) < people3.height / 2)
+            movable[3] = true;
+        if (abs(x - car1.x) < car1.width / 2 && abs(y - car1.y) < car1.height / 2)
+            movable[4] = true;
+        if (abs(x - car2.x) < car2.width / 2 && abs(y - car2.y) < car2.height / 2)
+            movable[5] = true;
+        if (abs(x - car3.x) < car3.width / 2 && abs(y - car3.y) < car3.height / 2)
+            movable[6] = true;
+        if (abs(x - car4.x) < car4.width / 2 && abs(y - car4.y) < car4.height / 2)
+            movable[7] = true;
+    }
 
+    //people1.x += 0;//这里写每个精灵对象的位置操作 在很小的时间间隔内执行一次这个函数
 
+    const MoveSpeed = 2;//默认移动速度为2
 
+    if (movable[0]) {//若处于运动状态，则根据方向进行运动
+        if (direction[0] == 0) people0.y -= MoveSpeed;
+        if (direction[0] == 90) people0.x += MoveSpeed;
+        if (direction[0] == 180) people0.y += MoveSpeed;
+        if (direction[0] == 270) people0.x -= MoveSpeed;
+    }
 
+    if (people0.x <= 85) {
+        direction[0] += 90;
+        direction[0] %= 360;
+        update_direction(0);
+        people0.x += 3;
+        
+    }//补全碰到其他边界和其他对象的情况
+    //补全其他对象的运动检测判断
 }
